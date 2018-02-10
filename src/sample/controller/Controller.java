7b68718eb95b0ad0b3b00e190c33e94dbfc98236
fbclol <boue.franck@orange.fr>
2018-02-10 18:40:05 +0100

    amélioration chooser

--- src/sample/controller/Controller.java
+++ src/sample/controller/Controller.java
@@ -1,4 +1,6 @@
 package sample.controller;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 
 import javax.swing.*;
@@ -8,9 +10,14 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.image.Image ;
 import javafx.scene.control.* ;
+import javafx.scene.layout.Pane;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class Controller {
     @FXML
@@ -19,25 +26,20 @@ public class Controller {
 //    private GridPane imgGrid;
     @FXML
     private ScrollPane imgScroll;
+    @FXML
+    private ImageView oneImg;
 
     public void Parcourir() {
         JFileChooser dialogue;
         System.out.println(chemin.getText());
-        if (chemin.getText() == "") {
-            dialogue = new JFileChooser();
-        } else {
-            dialogue = new JFileChooser(chemin.getText());
-        }
-        // création de la boîte de dialogue
-        dialogue = new JFileChooser();
-        //Recupération que des repertoire
-        dialogue.setFileSelectionMode(2);
-        // affichage
-        // todo: a enlever quand on rend le projet
-        dialogue.setCurrentDirectory(new File("./src/sample"));
-        dialogue.showOpenDialog(null);
-        if (dialogue.getSelectedFile().toString() != null) {
-            chemin.setText(dialogue.getSelectedFile().toString());
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Open File");
+//      chooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        chooser.setInitialDirectory(new File("./src/sample"));
+
+        File file = chooser.showDialog(new Stage());
+        if (file != null) {
+            chemin.setText(file.toString());
         }
 
         File repertoire = new File(chemin.getText());
@@ -64,14 +66,44 @@ public class Controller {
             String imageURI = new File(files[i].toString()).toURI().toString();
             Image image = new Image(imageURI, 400, 400, true, true);
             ImageView imageView = new ImageView(image);
-            imageView.setId(toString()+i);
+            imageView.setId("img_"+i);
 
             // ajout chaque image dans une cellule
             imgGrid.add(new ImageView(image),column,row);
+
+
+
         }
         // permet d'ajouter l'object imgGrig a l'object imgScroll pour scroller en fonction du nombre d'image
         imgScroll.setContent(imgGrid);
+        // afficher la grille
+        imgGrid.setGridLinesVisible(true);
+        // mettre des espaces entre les cellules
+        imgGrid.setHgap(6);
+        imgGrid.setVgap(6);
+
+        // parti loupe
+        // récupére la premier image
+        ImageView nodeImage1 = (ImageView) imgGrid.getChildren().get(0);
+        oneImg.setImage(nodeImage1.getImage());
+        // todo : si on veut récupérer un element via l'id ( à tester)
+        // ImageView photoView = (ImageView)node.lookup("#img_1");
+
+//        imgGrid.getChildren().setOnMouseClicked ( new EventHandler <MouseEvent>() {
+//            public void handle ( MouseEvent event ) {
+//
+//                Node node = (Node) event.getSource();
+//                System.out.print(node);
+////                ImageView photoView = (ImageView)node.lookup("#"+node.getId());
+////System.out.print(node);
+////                oneImg.setImage(photoView.getImage());
+//            }
+//        });
+
+
     }
+
+
 
     private static FilenameFilter jpgFileFilter = new FilenameFilter() {
         public boolean accept(File dir, String name) {
